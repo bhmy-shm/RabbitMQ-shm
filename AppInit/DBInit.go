7113e445f3ae3f6b9049414c23f87933a9e8d6f0
bhmy-shm <1136265636@qq.com>
2021-03-02 10:18:37 +0800

    初始化MQ,DB连接

---
+++ AppInit/DBInit.go
@@ -0,0 +1,35 @@
+package AppInit
+
+import (
+	"fmt"
+
+	// _ "github.com/go-sql-driver/mysql"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
+)
+
+var db *sqlx.DB
+
+func DBInit() error {
+	var err error
+	//数据库连接信息
+	dsn := "root:123.com@tcp(127.0.0.1:3306)/userregister?charset=utf8mb4&parseTime=True&loc=Local"
+
+	//连接数据库
+	db, err = sqlx.Connect("mysql", dsn)
+	if err != nil {
+		fmt.Printf("dsn:%s failed err :%v\n", dsn, err)
+		return err
+	}
+
+	//设置数据库的最大连接数,设置为10
+	db.SetMaxOpenConns(10)
+	//最大空闲连接数
+	db.SetMaxIdleConns(5)
+
+	return nil
+}
+
+func GetDB() *sqlx.DB {
+	return db
+}
